d870821f7eb8950205109d7f384d4e8cb31c410a
abidibo <abidibo@gmail.com>
2025-01-22 17:41:35 +0100

    feat: add tools help text

--- src/Components/RoiEditor/Toolbar.tsx
+++ src/Components/RoiEditor/Toolbar.tsx
@@ -7,33 +7,38 @@ import RectangleIcon from '../../Icons/RectangleIcon'
 import { useEditorContext } from '../../Providers/EditorProvider'
 import { UiContext } from '../../Providers/UiProvider'
 import { css } from '../../Utils'
+import ColorPicker from './ColorPicker'
 import styles from './Toolbar.module.css'
 import { ToolEnum } from './Types'
-import ColorPicker from './ColorPicker'
 
 const Toolbar = () => {
-  const { IconButton, themeMode, primaryColor } = useContext(UiContext)
+  const { IconButton, themeMode, primaryColor, Typography, strings } = useContext(UiContext)
   const { activeTool, setActiveTool } = useEditorContext()
 
   const iconColor = (tool: ToolEnum) => (tool === activeTool ? primaryColor : themeMode === 'light' ? 'black' : 'white')
   const setTool = (tool: ToolEnum) => () => setActiveTool(tool)
 
   return (
-    <div className={css('toolbar', styles, themeMode)}>
-      <IconButton onClick={setTool(ToolEnum.Pointer)}>
-        <PointerIcon color={iconColor(ToolEnum.Pointer)} />
-      </IconButton>
-      <IconButton onClick={setTool(ToolEnum.Polyline)}>
-        <PolylineIcon color={iconColor(ToolEnum.Polyline)} />
-      </IconButton>
-      <IconButton onClick={setTool(ToolEnum.Polygon)}>
-        <PolygonIcon color={iconColor(ToolEnum.Polygon)} />
-      </IconButton>
-      <IconButton onClick={setTool(ToolEnum.Rectangle)}>
-        <RectangleIcon color={iconColor(ToolEnum.Rectangle)} />
-      </IconButton>
-      <ColorPicker style={{ marginLeft: 'auto' }} />
-    </div>
+    <>
+      <div className={css('toolbar', styles, themeMode)}>
+        <IconButton onClick={setTool(ToolEnum.Pointer)}>
+          <PointerIcon color={iconColor(ToolEnum.Pointer)} />
+        </IconButton>
+        <IconButton onClick={setTool(ToolEnum.Polyline)}>
+          <PolylineIcon color={iconColor(ToolEnum.Polyline)} />
+        </IconButton>
+        <IconButton onClick={setTool(ToolEnum.Polygon)}>
+          <PolygonIcon color={iconColor(ToolEnum.Polygon)} />
+        </IconButton>
+        <IconButton onClick={setTool(ToolEnum.Rectangle)}>
+          <RectangleIcon color={iconColor(ToolEnum.Rectangle)} />
+        </IconButton>
+        <ColorPicker style={{ marginLeft: 'auto' }} />
+      </div>
+      <div className={css('toolbar-helper', styles, themeMode)}>
+        <Typography>{strings[activeTool]}: {strings[`${activeTool}HelpText`]}</Typography>
+      </div>
+    </>
   )
 }
 
