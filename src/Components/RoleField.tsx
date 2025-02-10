4c0bacfbf0709323f9db1852d6af13a33445fa5f
abidibo <abidibo@gmail.com>
2025-02-10 12:15:29 +0100

    feat: add role field do not show not allowed options (multiplicity)

--- src/Components/RoleField.tsx
+++ src/Components/RoleField.tsx
@@ -3,18 +3,33 @@ import { useContext, useEffect } from 'react'
 import { useEditorContext } from '../Providers/EditorProvider'
 import { UiContext } from '../Providers/UiProvider'
 import { FieldProps } from '../Types'
-import EnumField from './EnumField'
 import { humanize } from '../Utils'
-import { ShapeType } from './RoiEditor/Types'
+import EnumField from './EnumField'
+import { Metadata, OperatorEnum, ShapeType } from './RoiEditor/Types'
 
-type RoleFieldProps = Omit<FieldProps<string>, 'readonly' | 'label'> & { shapeType: ShapeType}
+type RoleFieldProps = Omit<FieldProps<string>, 'readonly' | 'label'> & { shapeType: ShapeType }
+
+const isAllowed = (role: string, multiplicity: { operator: string; threshold: number }, metadata: Metadata) => {
+  switch (multiplicity.operator) {
+    case OperatorEnum.Eq:
+    case OperatorEnum.Lte:
+      return metadata.rois.filter((m) => m.role === role).length < multiplicity.threshold
+    case OperatorEnum.Lt:
+      return metadata.rois.filter((m) => m.role === role).length < multiplicity.threshold - 1
+    default:
+      return true
+  }
+}
 
 const RoleField: React.FC<RoleFieldProps> = ({ onChange, value, required, shapeType, ...props }) => {
   const { strings } = useContext(UiContext)
-  const { configuration } = useEditorContext()
+  const { configuration, metadata } = useEditorContext()
   const options: string[] = []
-    ; (configuration.rois || []).filter(r => r.type === shapeType).forEach((r) => {
-      if (!options.includes(r.role)) {
+  const rois = configuration.rois || []
+  rois
+    .filter((r) => r.type === shapeType)
+    .forEach((r) => {
+      if (!options.includes(r.role) && (!r.multiplicity || isAllowed(r.role, r.multiplicity!, metadata))) {
         options.push(r.role)
       }
     })
@@ -22,7 +37,7 @@ const RoleField: React.FC<RoleFieldProps> = ({ onChange, value, required, shapeT
   useEffect(() => {
     if (required) {
       onChange(options[0])
-    }    
+    }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
