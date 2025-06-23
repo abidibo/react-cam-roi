6c174f09677e6f9773b250ae0b9f590c5f65c6f0
abidibo <abidibo@gmail.com>
2025-06-23 16:14:45 +0200

    fix: tooltip props

--- src/Components/Tooltip/index.tsx
+++ src/Components/Tooltip/index.tsx
@@ -1,6 +1,6 @@
 type TooltipProps = {
   title: string
-  children: React.ReactNode
+  children: React.ReactElement<unknown, any>
 }
 const Tooltip: React.FC<TooltipProps> = ({ title, children }) => <div title={title}>{children}</div>
 
